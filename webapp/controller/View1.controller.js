811dbc6c2c1a15fe0f92072678b68ea8262aa5ad
duangww <duangww@live.com>
2025-06-26 15:38:09 +0800

    增加周指标

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -30,21 +30,15 @@ sap.ui.define([
             var formatPattern = ChartFormatter.DefaultPattern;
                 var oVizFrame = this.byId("idVizFrame");
                 var oVizFrame2 = this.byId("idVizFrame2");
+                var oVizFrame3 = this.byId("idVizFrame3"); 
+                var oVizFrame4 = this.byId("idVizFrame4"); 
                 this._setVizFrameProperties(oVizFrame,formatPattern);
                 this._setVizFrameProperties(oVizFrame2,formatPattern);
+                this._setVizFrameProperties(oVizFrame3,formatPattern);
+                this._setVizFrameProperties(oVizFrame4,formatPattern);
               
-                var cardManifests = new JSONModel(),
-                    homeIconUrl = sap.ui.require.toUrl("zfiorifi01/images/CompanyLogo.png"),
-                    date = DateFormat.getDateInstance({ style: "long" }).format(UI5Date.getInstance());
-
-                cardManifests.loadData(sap.ui.require.toUrl("zfiorifi01/model/cardManifests.json"));
-
-                this.getView().setModel(cardManifests, "manifests");
-                this.getView().setModel(new JSONModel({
-                    homeIconUrl: homeIconUrl,
-                    date: date
-                }));
-                var oFiData = new JSONModel();
+              
+               
                  // 设置 Basic Auth 用户名和密码
                 // var sUser = "";
                 // var sPassword = "";
@@ -79,22 +73,23 @@ sap.ui.define([
                                 yearMonth: item.year + "/" + formattedMonth
                             };
                         })
+                        const oFiData = new JSONModel();
                         oData1[0].data = oData2;
                         oFiData.setData(oData1);
                         oView.setModel(oFiData, "FiData");
-                        console.log(oFiData.getData());
                        
                         var oModel = this.getView().getModel("FiData");
                         var aData = oModel.getProperty("/0/data");
                     
                         // 初始渲染时判断屏幕宽度
-                        this._setChartDataByScreen(aData);                                       
+                        this._setChartDataByScreen(aData,"FiData");                                       
                         // 监听窗口大小变化
                         window.addEventListener("resize", () => {
                             var oModel = this.getView().getModel("FiData");
                             var aData = oModel.getProperty("/0/data");
-                            this._setChartDataByScreen(aData);
+                            this._setChartDataByScreen(aData,"FiData");
                         });
+
                     },
                     error: function (jqXHR, sTextStatus, sError) {
                         console.error("错误详情:", jqXHR.responseText);
@@ -102,8 +97,15 @@ sap.ui.define([
                 });
 
             var oPopOver = this.getView().byId("idPopOver");
-            oPopOver.connect(oVizFrame.getVizUid());
-            oPopOver.setFormatString(formatPattern.STANDARDFLOAT);
+            var oPopOver2 = this.getView().byId("idPopOver2");
+            var oPopOver3 = this.getView().byId("idPopOver3");
+            var oPopOver4 = this.getView().byId("idPopOver4");
+            this._setoPopOver(oPopOver,oVizFrame,formatPattern);
+            this._setoPopOver(oPopOver2,oVizFrame2,formatPattern);
+            this._setoPopOver(oPopOver3,oVizFrame3,formatPattern);
+            this._setoPopOver(oPopOver4,oVizFrame4,formatPattern);
+            this._getWeekData(oView,oPayload);
+
             },
             _setVizFrameProperties: function (oVizFrame,formatPattern) {
                 // 隐藏标题
@@ -130,6 +132,12 @@ sap.ui.define([
                             title: {
                                 visible: false
                             }
+                        },
+                        categoryAxis: {
+                            label: {
+                                rotation: "45",
+                                angle: 45
+                            }
                         }
                     });
                 }
@@ -141,8 +149,8 @@ sap.ui.define([
              * @param {Array} aData - 原始数据数组
              * @private
              */
-            _setChartDataByScreen: function(aData) {
-                var oModel = this.getView().getModel("FiData");
+            _setChartDataByScreen: function(aData,ModelName) {
+                var oModel = this.getView().getModel(ModelName);
                 var width = window.innerWidth;
                 var aShowData;
             
@@ -150,12 +158,68 @@ sap.ui.define([
                     // 小屏幕只显示最近4个月
                     aShowData = aData.slice(-4);
                 } else {
-                    // 大屏幕显示最近10个月
+                    // 大屏幕显示最近8个月
                     aShowData = aData.slice(-8);
                 }
                 // 设置到新路径，供图表绑定
-                oModel.setProperty("/0/chartData", aShowData);
-                            
+                switch(ModelName){
+                    case "FiData": oModel.setProperty("/0/chartData", aShowData);
+                    break;
+                    case "WeekData":oModel.setProperty("/0/chartWeekData", aShowData);
+                    break;
+                }                
+            },
+            _setoPopOver:function(oPopOver,oVizFrame,formatPattern){
+                oPopOver.connect(oVizFrame.getVizUid());
+                oPopOver.setFormatString(formatPattern.STANDARDFLOAT);
+            },
+            _getWeekData:function(oView,oPayload){
+                $.ajax({
+                    url: "/zbak_inf?ACTION=GET_WEEK",
+                    method: "POST",
+                    contentType: "application/json",
+                    // headers: {
+                    //     "Authorization": "Basic " + sAuth
+                    // },
+                    data: JSON.stringify(oPayload),
+                    success: (oData)=> {
+                        //    MessageToast.show("API调用成功");
+                        const oWeekData = new JSONModel();
+                        const oData1 = JSON.parse(oData);
+                        const oData2 = oData1[0].data.map(item => {
+                            const weekStr = String(item.week);
+                            const year = weekStr.substring(0,4);
+                            const week =  weekStr.substring(4,6);
+                            return {
+                                ...item,
+                                yearWeek:`${year}/${week}`
+                            };
+                        });
+                        oData1[0].data = oData2;
+                        oWeekData.setData(oData1);
+                        oView.setModel(oWeekData, "WeekData");
+                       
+                        var oModel = oView.getModel("WeekData");
+                        var aData = oModel.getProperty("/0/data");
+                    
+                        // 初始渲染时判断屏幕宽度
+                        this._setChartDataByScreen(aData,"WeekData");                                       
+                        // 监听窗口大小变化
+                        window.addEventListener("resize", () => {
+                            const oModel = this.getView().getModel("WeekData");
+                            const aData = oModel.getProperty("/0/data");
+                            this._setChartDataByScreen(aData,"WeeData");
+                        });
+
+                        var oVizFrame3 = this.getView().byId("idVizFrame3");
+                        var formatPattern = ChartFormatter.DefaultPattern;
+                        this._setVizFrameProperties(oVizFrame3, formatPattern);
+                    },
+                    error: function (jqXHR, sTextStatus, sError) {
+                        console.error("错误详情:", jqXHR.responseText);
+                    }
+                });
+
             }
         });
     });
